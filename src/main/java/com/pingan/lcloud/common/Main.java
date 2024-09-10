2061ff642e76a14fb389c130243fe0fb66886b9c
xiaojiakunx <xiaojiakunx@gmail.com>
2024-09-10 20:27:54 +0800

    frist

---
+++ src/main/java/com/pingan/lcloud/common/Main.java
@@ -0,0 +1,12 @@
+package com.pingan.lcloud.common;
+
+/**
+ * @author: xj
+ * @version: 2024/9/10
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("这是GBK需要测试");
+    }
+}
